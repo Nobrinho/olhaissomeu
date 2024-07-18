613d95f7bf03e98ed3f3cb143692d17a688bd023
Nobrinho <emerson_14@live.com>
2024-07-18 14:18:20 -0400

    fix dependences

--- src/layout/footer/index.tsx
+++ src/layout/footer/index.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <div className='footer'>
       <Grid container>
         <Grid item xs={12} sm={6} sx={{ display: 'flex', alignItems: 'center', justifyContent:'center', padding: "5px 50px 0px 50px"}}>
-          <Typography variant="h6" sx={{ color: 'white' }}>© 2024 - Todos os direitos reservados - OlhaIssoMeu. Clique ao lado e saiba mais</Typography>
+          <Typography variant="h6" sx={{ color: 'white' }}>© 2024 - Todos oss direitos reservados - OlhaIssoMeu. Clique ao lado e saiba mais</Typography>
         </Grid>
         <Grid item xs={12} sm={6} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
           <div style={{ background: "white", padding: "5px 70px 0px 70px", borderRadius: "22px", margin: '20px' }}>
